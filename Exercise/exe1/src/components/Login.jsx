4aaa8d2a6df9b03d8b811eeadb5d66a51399a13e
surajpxl <guptasuraj4455@gmail.com>
2025-08-03 23:19:58 +0530

    some add

--- Exercise/exe1/src/components/Login.jsx
+++ Exercise/exe1/src/components/Login.jsx
@@ -1,27 +1,31 @@
-import React, { useState } from 'react'
-import contextProvider from '../context/contextProvider'
-import { useContext } from 'react'
+import React, {useState, useContext} from 'react'
+import UserContext from '../context/UserContext'
 
 function Login() {
-    const[username, setUsername] = useState("")
-    const[password, setPassword] = useState("")
-const {setUser} = useContext(contextProvider)
+  const [username, setUsername] = useState('')
+  const [password, setpassword] = useState('')
 
-    handleSubmit = (e) => {
-        e.preventDefault()
-        setUser(username, password)
-    }
+  const{setUser} = useContext(UserContext)
+
+const handleSubmit = (e) => {
+      e.preventDefault()
+      setUser({username, password})
+}
+
   return (
     <div>
-      <h1>
-        Login Page
-      </h1>
-      <form action="">
-        <input value={username} onChange={(e)=>setUsername(e.target.value)} type="text" placeholder='Enter username' />
-        
-        <input value={password} onChange={(e)=>setPassword(e.target.value)} type="password" placeholder='Enter password' />
-        <button onClick={handleSubmit}>submit</button>
-      </form>
+      <h2>Login</h2>
+      <input type='text' 
+      value={username}
+      onChange={(e) => setUsername(e.target.value)}
+      placeholder='username'/>
+      {" "}
+      <input type='text'
+      value={password}
+      onChange={(e) => setpassword(e.target.value)}
+      placeholder='password'/>
+      <button onClick={handleSubmit}>Submit</button>
+      
     </div>
   )
 }
